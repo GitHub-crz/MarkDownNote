554bf0c334e9ca9d8edb54a960a68fd0d4b5aad9
rzcai <2437096155@qq.com>
2022-04-29 22:32:21 +0800

    鑷姩鎻愪氦

---
+++ automatic.bat
@@ -0,0 +1,25 @@
+@echo off
+
+echo.
+echo                                      GitHub自动上传脚本
+echo ===================================================================================
+echo.
+
+set /p branch=请输入你的分支名：
+set /p change=请输入commit名：
+echo.
+D:
+cd MarkDownNote
+
+git pull
+git add .
+git commit -m "自动提交"
+git push
+
+echo.
+echo ===================================================================================
+echo                                      更新完毕
+echo.
+
+pause
+
